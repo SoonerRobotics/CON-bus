457374fd6e234956d04e9124a96964062dd292fc
Tyler Julian <Tyler.James.Julian@ou.edu>
2023-04-08 16:50:00 -0500

    create idea of UI look

--- CONBusUI/main.py
+++ CONBusUI/main.py
@@ -1,4 +1,9 @@
 import sys
+
+import serial.tools.list_ports
+import can
+import json
+
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -8,10 +13,17 @@ from PyQt5 import uic
 class mainWindow(QMainWindow):
     def __init__(self):
         super(mainWindow, self).__init__()
-        uic.loadUi("CONbus.ui", self)  # Load the .ui
+        uic.loadUi("./CONBusUI/CONbus.ui", self)  # Load the .ui
+
+        self.update_coms()
 
         self.show()
 
+    def update_coms(self):
+        ports = serial.tools.list_ports.comports()
+        print("something")
+        for port, desc, hwid in sorted(ports):
+            print("PORT: ", port)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
