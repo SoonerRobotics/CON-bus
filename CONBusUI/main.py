1ca8b46f7a572d54d7367e4332f0a9c58b706237
Tyler Julian <Tyler.James.Julian@ou.edu>
2023-04-09 12:27:08 -0500

    json example

--- CONBusUI/main.py
+++ CONBusUI/main.py
@@ -16,14 +16,55 @@ class mainWindow(QMainWindow):
         uic.loadUi("./CONBusUI/CONbus.ui", self)  # Load the .ui
 
         self.update_coms()
+        
+        self.updateComButton.released.connect(self.update_coms)
+
+        self.connected = False
+        self.canbus = None
+        self.connectButton.released.connect(self.toggle_connection)
+        self.connectButton.setStyleSheet("background-color: #FFCCCB")
+
+        self.con_write("STATUS BOX", False)
 
         self.show()
 
+    def toggle_connection(self):
+        if self.connected:
+            self.connectButton.setStyleSheet("background-color: #FFCCCB")
+            self.connectButton.setText("DISCONNECTED")
+            self.con_write("DISCONNECTED", False)
+            self.connected = False
+            self.canbus = None
+        else:
+            try:
+                comPort = self.comComboBox.currentText()
+                bitRate = self.baudrateSpinbox.value() * 1000
+                self.canbus = can.interface.Bus(bustype='slcan', channel=comPort, bitrate=bitRate)
+
+                self.connectButton.setStyleSheet("background-color: lightgreen")
+                self.connectButton.setText("CONNECTED")
+                self.con_write("CONNECTED", False)
+                self.connected = True
+            except Exception as e:
+                self.con_write("CONBUS CONNECTION ERROR: " + str(e))
+
+
+    def con_write(self, value = None, appendText = True):
+        if value is not None and appendText is True:
+            self.statusBox.append(value)
+        elif value is not None and appendText is False:
+            self.statusBox.clear()
+            self.statusBox.append(value)
+        else:
+            self.statusBox.clear()
+
     def update_coms(self):
         ports = serial.tools.list_ports.comports()
-        print("something")
+        self.comComboBox.clear()
+        self.comComboBox.addItem("SELECT CAN to USB DEVICE")
+        self.statusBox.append("COM LIST UPDATED")
         for port, desc, hwid in sorted(ports):
-            print("PORT: ", port)
+            self.comComboBox.addItem(port)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
