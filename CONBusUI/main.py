e18364d17a68f1a7f58a2c148505b71d9e4f0f21
Tyler Julian <Tyler.James.Julian@ou.edu>
2023-04-10 06:14:06 -0500

    open json

--- CONBusUI/main.py
+++ CONBusUI/main.py
@@ -24,7 +24,12 @@ class mainWindow(QMainWindow):
         self.connectButton.released.connect(self.toggle_connection)
         self.connectButton.setStyleSheet("background-color: #FFCCCB")
 
-        self.con_write("STATUS BOX", False)
+        self.parameters = {}
+        self.openRegisterValueButton.released.connect(self.open_json)
+
+        self.con_print("STATUS BOX", False)
+
+        
 
         self.show()
 
@@ -32,7 +37,7 @@ class mainWindow(QMainWindow):
         if self.connected:
             self.connectButton.setStyleSheet("background-color: #FFCCCB")
             self.connectButton.setText("DISCONNECTED")
-            self.con_write("DISCONNECTED", False)
+            self.con_print("DISCONNECTED", False)
             self.connected = False
             self.canbus = None
         else:
@@ -43,13 +48,28 @@ class mainWindow(QMainWindow):
 
                 self.connectButton.setStyleSheet("background-color: lightgreen")
                 self.connectButton.setText("CONNECTED")
-                self.con_write("CONNECTED", False)
+                self.con_print("CONNECTED", False)
                 self.connected = True
             except Exception as e:
-                self.con_write("CONBUS CONNECTION ERROR: " + str(e))
+                self.con_print("CONBUS CONNECTION ERROR: " + str(e))
 
+    def open_json(self):
+        try:
+            bin = QFileDialog.getOpenFileName(
+                self, "Open device json file", ".", "*.json" 
+            )
+            if bin[0] != "":
+                file = open(bin[0])
+                self.parameters = json.load(file)
+                
+                for key in self.parameters.keys():
+                    self.changeTable.add
+                
+                self.con_print("JSON OPEN SUCCESSFULLY")
+        except Exception as e:
+            self.con_print("JSON file open FAIL: " + str(e))
 
-    def con_write(self, value = None, appendText = True):
+    def con_print(self, value = None, appendText = True):
         if value is not None and appendText is True:
             self.statusBox.append(value)
         elif value is not None and appendText is False:
